e1ffebc9d43d37c28ef7334cbf2a271ec167b8da
dpslwk <dps.lwk@gmail.com>
2017-10-17 13:26:58 +0100

    dep version bump and trying Perfect Assistant 3 for linux build

--- Package.swift
+++ Package.swift
@@ -1,25 +1,19 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// Generated automatically by Perfect Assistant 2
+// Date: 2017-10-17 00:38:05 +0000
 import PackageDescription
 
 let package = Package(
-    name: "nh-lighting",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "2.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-WebSockets.git", from: "2.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-Mosquitto", from: "1.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-MariaDB.git", from: "2.0.0"),
-        .package(url: "https://github.com/IBM-Swift/BlueSignals.git", from: "0.0.0"),
-        .package(url: "https://github.com/IBM-Swift/Configuration.git", from: "1.0.0")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "nh-lighting",
-            dependencies: ["PerfectMosquitto", "Signals", "PerfectHTTPServer", "PerfectWebSockets", "MariaDB", "Configuration"]),
-    ]
+	name: "nh-lighting",
+	dependencies: [
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-WebSockets.git", "3.0.0"..<"4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Mosquitto", "1.0.0"..<"2.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-MariaDB.git", "3.0.0"..<"4.0.0"),
+		.package(url: "https://github.com/IBM-Swift/BlueSignals.git", "0.0.0"..<"1.0.0"),
+		.package(url: "https://github.com/IBM-Swift/Configuration.git", "1.0.0"..<"2.0.0")
+	],
+	targets: [
+		.target(name: "nh-lighting", dependencies: ["PerfectMosquitto", "Signals", "PerfectHTTPServer", "PerfectWebSockets", "MariaDB", "Configuration"])
+	]
 )
