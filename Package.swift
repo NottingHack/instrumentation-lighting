5cf9537c68d04b6decc583d2e1494510cf020313
dpslwk <dps.lwk@gmail.com>
2017-10-20 22:23:22 +0100

    Bump Mosquitto dependancy

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:4.0
 // Generated automatically by Perfect Assistant 2
-// Date: 2017-10-17 00:38:05 +0000
+// Date: 2017-10-20 21:06:05 +0000
 import PackageDescription
 
 let package = Package(
@@ -8,7 +8,7 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-WebSockets.git", "3.0.0"..<"4.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Mosquitto", "1.0.0"..<"2.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Mosquitto", "3.0.0"..<"4.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-MariaDB.git", "3.0.0"..<"4.0.0"),
 		.package(url: "https://github.com/IBM-Swift/BlueSignals.git", "0.0.0"..<"1.0.0"),
 		.package(url: "https://github.com/IBM-Swift/Configuration.git", "1.0.0"..<"2.0.0")
